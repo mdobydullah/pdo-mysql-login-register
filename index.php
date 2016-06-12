dbd5e693fa0630d7f4186996d399f940c4229602
Md. Obydullah <mdobydullah@users.noreply.github.com>
2016-06-12 21:24:02 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-<head><title>PDO Login & Register Using PDO MySQL</title></head>
+<head><title>PDO MySQL</title></head>
 	<style>
 	html, body {
 		margin: 1px;
@@ -8,15 +8,19 @@
 	</style>
 <body>
 	<div align="center">
-		<div style=" border: solid 1px #00796b; " align="left">
+		<div style=" border: solid 1px #079B96; " align="left">
 			<?php
 				if(isset($errMsg)){
 					echo '<div style="color:#FF0000;text-align:center;font-size:18px;">'.$errMsg.'</div>';
 				}
 			?>
-			<div style="background-color:#00796b; color:#FFFFFF; padding:10px;"><b>PDO MySQL</b></div>
+			<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>PDO MySQL</b></div>
 			<div style="margin: 15px">
-				Welcome to iTnetBD !
+				Welcome guest !
+				<br>
+				<a href="login.php">Login</a> <br>
+				<a href="register.php">Register</a> <br>
+				<a href="forgot.php">Forgot Password</a>
 			</div>
 		</div>
 	</div>
