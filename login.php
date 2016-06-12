737f2cf015ff378e6e2bc9b49595246f3d4ea130
Md. Obydullah <mdobydullah@users.noreply.github.com>
2016-06-12 21:26:01 +0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,75 @@
+<?php
+	require 'config.php';
+
+	if(isset($_POST['login'])) {
+		$errMsg = '';
+
+		// Get data from FORM
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		if($username == '')
+			$errMsg = 'Enter username';
+		if($password == '')
+			$errMsg = 'Enter password';
+
+		if($errMsg == '') {
+			try {
+				$stmt = $connect->prepare('SELECT id, fullname, username, password, secretpin FROM pdo WHERE username = :username');
+				$stmt->execute(array(
+					':username' => $username
+					));
+				$data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+				if($data == false){
+					$errMsg = "User $username not found.";
+				}
+				else {
+					if($password == $data['password']) {
+						$_SESSION['name'] = $data['fullname'];
+						$_SESSION['username'] = $data['username'];
+						$_SESSION['password'] = $data['password'];
+						$_SESSION['secretpin'] = $data['secretpin'];
+
+						header('Location: dashboard.php');
+						exit;
+					}
+					else
+						$errMsg = 'Password not match.';
+				}
+			}
+			catch(PDOException $e) {
+				$errMsg = $e->getMessage();
+			}
+		}
+	}
+?>
+
+<html>
+<head><title>Login</title></head>
+	<style>
+	html, body {
+		margin: 1px;
+		border: 0;
+	}
+	</style>
+<body>
+	<div align="center">
+		<div style=" border: solid 1px #006D9C; " align="left">
+			<?php
+				if(isset($errMsg)){
+					echo '<div style="color:#FF0000;text-align:center;font-size:17px;">'.$errMsg.'</div>';
+				}
+			?>
+			<div style="background-color:#006D9C; color:#FFFFFF; padding:10px;"><b>Login</b></div>
+			<div style="margin: 15px">
+				<form action="" method="post">
+					<input type="text" name="username" value="<?php if(isset($_POST['username'])) echo $_POST['username'] ?>" autocomplete="off" class="box"/><br /><br />
+					<input type="password" name="password" value="<?php if(isset($_POST['password'])) echo $_POST['password'] ?>" autocomplete="off" class="box" /><br/><br />
+					<input type="submit" name='login' value="Login" class='submit'/><br />
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
